952b62d06c0799fe41b0c651c717257809514de1
ShahYash21 <shahyashm21@gmail.com>
2026-06-25 23:59:54 +0530

    Implement alternative string rotation method
    Added an alternative method for rotating the string.

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -4,3 +4,11 @@ class Solution:
             return False
 
         return goal in (s+s)
+        """
+        also do it below way(after 4 th line)
+        for i in range(len(s)):
+            s = s[1:] + s[0]
+            if s == goal:
+                return True
+        return False      
+        """
