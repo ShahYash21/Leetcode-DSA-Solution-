7ff60c0ad667ec2000ea4167640fea4712c0a39d
ShahYash21 <shahyashm21@gmail.com>
2026-06-27 00:04:56 +0530

    Simple approch for solving  moveZeroes problem
    Refactor moveZeroes function to use a temporary list for zero handling.

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,3 +7,15 @@ class Solution:
             if nums[right] != 0:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
+                """" this is the simple approach
+                def moveZeroes(self, nums: List[int]) -> None:
+                    t = []
+
+                    for num in nums:
+                        if num != 0:
+                        t.append(num)
+
+                    while len(t) < len(nums):
+                            t.append(0)
+
+                    nums[:] = t"""
